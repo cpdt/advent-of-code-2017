61d275b7bede4c074dabacf0315b34a074c5b509
cpdt <copodt@gmail.com>
2017-12-03 14:06:15 +1000

    Rewrite day 2 solution with iterator

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 
-fn get_line_checksum(nums: &Vec<i32>) -> i32 {
+fn get_line_checksum(nums: Vec<i32>) -> i32 {
     let max_num = nums.iter().fold(0, |acc, &x| std::cmp::max(acc, x));
     let min_num = nums.iter().fold(max_num, |acc, &x| std::cmp::min(acc, x));
 
     max_num - min_num
 }
 
-fn get_line_division(nums: &Vec<i32>) -> i32 {
+fn get_line_division(nums: Vec<i32>) -> i32 {
     for (i, a) in nums.iter().enumerate() {
+        // only iterate through numbers after the current one, gives a bit more efficiency
+        // (but we have to check both ways inside here)
         for b in nums.iter().skip(i + 1) {
             if a % b == 0 {
                 return a / b
@@ -21,22 +23,43 @@ fn get_line_division(nums: &Vec<i32>) -> i32 {
     0
 }
 
+struct LineReader;
+
+impl Iterator for LineReader {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        let mut val = String::new();
+        match io::stdin().read_line(&mut val) {
+            Err(_) => None,
+            Ok(_) => {
+                let trimmed_val = val.trim();
+
+                if trimmed_val.len() > 1 {
+                    Some(trimmed_val.to_owned())
+                } else {
+                    None
+                }
+            }
+        }
+    }
+}
+
+fn line_reader() -> LineReader {
+    LineReader
+}
+
 fn main() {
     println!("Please enter the spreadsheet lines, followed by an empty line:");
 
-    let mut checksum = 0;
-    let mut val = String::new();
-    while {
-        io::stdin().read_line(&mut val).expect("Failed to read line");
-        val.len() > 1
-    } {
-        let nums = val.trim().split("\t").filter_map(|x| x.parse::<i32>().ok()).collect::<Vec<i32>>();
+    let lines = line_reader();
+    let checksum = lines.fold(0, |acc, line| {
+        let nums = line.split("\t").filter_map(|x| x.parse::<i32>().ok()).collect::<Vec<i32>>();
 
         // use get_line_checksum for first challenge
         // use get_line_division for second challenge
-        checksum += get_line_checksum(&nums);
-        val.clear();
-    }
+        acc + get_line_checksum(nums)
+    });
 
     println!("Spreadsheet checksum is {}", checksum);
 }
